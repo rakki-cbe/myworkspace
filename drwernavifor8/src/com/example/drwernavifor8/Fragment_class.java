97f7bcf0f80fa95de7523f80756428d8d2b2f322
Radhakrishanan <radhakrishnan.s@attinadsoftware.com>
2014-04-29 10:27:05 +0530

    test coomit

---
+++ drwernavifor8/src/com/example/drwernavifor8/Fragment_class.java
@@ -0,0 +1,33 @@
+package com.example.drwernavifor8;
+
+
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class Fragment_class extends android.support.v4.app.Fragment implements MainActivity.OnHeadlineSelectedListener {
+	TextView tv;
+	 
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		View root=inflater.inflate(R.layout.fragment_main, container,
+				false);
+		tv=(TextView) root.findViewById(R.id.updation);
+		tv.setText("Default");
+		return root;
+	}
+
+	@Override
+	public void onArticleSelected(String position) {
+		// TODO Auto-generated method stub
+		tv.setText("OPtion "+position+" selected");
+	}
+
+	
+	
+}
